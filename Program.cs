33e2174ac6b88ec9e74412c2ed2ccad657f4cdc0
davidovska <59517731+davidovska@users.noreply.github.com>
2020-03-19 14:43:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework03extras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
